8027d9d8c7b9e449fb81c9d51b8046c7e46c6d8e
C0pyR1ght <luca.feiser@gmail.com>
2020-07-27 18:30:23 +0200

    limit order results

--- handlers/orders.js
+++ handlers/orders.js
@@ -1,14 +1,17 @@
 'use strict';
 
 module.exports = function (req, res) {
+    const limit = req.query.limit ? parseInt(req.query.limit) : null;
+    let query;
+    if (limit && Number.isInteger(limit)) {
+        query = "SELECT * FROM `pos_orders` ORDER BY `pos_order_id` DESC LIMIT " + limit;
+    } else {
+        query = "SELECT * FROM `pos_orders` ORDER BY `pos_order_id` DESC";
+    }
     connection.query(
-        'SELECT * FROM `pos_orders` ORDER BY `pos_order_id` DESC',
+        query,
         function (err, results) {
             if (err) req.status(500).end();
-            results.map((row) => {
-                row.imgsrc = process.env.PRODUCTIMG_BASEURI + row.imgsrc;
-                row.price = row.price + ' €';
-            });
             res.json(results);
         }
     );
