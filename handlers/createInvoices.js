95fb4bb0de5ed2e4970eeacdb8759747de3e1730
C0pyR1ght <luca.feiser@gmail.com>
2020-08-09 12:47:50 +0200

    invoice creation

--- handlers/createInvoices.js
+++ handlers/createInvoices.js
@@ -1,18 +1,55 @@
 'use strict';
 
+const InvoiceService = require("../lib/InvoiceService");
+
 module.exports = function (req, res) {
+    var invoice = {
+        logo: "https://zentrale-online.org/pos-logo.png",
+        from: "Luca Feiser\nSchafgasse 1 St\n63500 Seligenstadt",
+        currency: "eur",
+        number: "ZENTRALE-0001",
+        due_date: "28.07.1999",
+        notes: "Zahlung Bar oder per PayPal: paypal.me/zentralepos",
+    };
+
+
     connection.query(
-        'SELECT * FROM `pos_orders` ORDER BY `pos_order_id` DESC',
+        'SELECT * FROM pos_accounts;',
         function (err, results, fields) {
-          if (err) {
-            console.error(err);
-            return res.status(500).end();
-          }
-            results.map((row) => {
-                row.imgsrc = process.env.PRODUCTIMG_BASEURI + row.imgsrc;
-                row.price = row.price + ' €';
+            if (err) {
+                console.error(err);
+                return res.status(500).end();
+            }
+
+            results.forEach(account =>  {
+
+                invoice.to = account.name;
+
+                connection.query(
+                    'SELECT count(*) AS quantity, name, price as unit_cost FROM `zentrale-pos`.pos_orders\n' +
+                    'LEFT JOIN pos_products ON pos_orders.pos_product_id = pos_products.pos_products_id\n' +
+                    'WHERE pos_account_id = '+ account.pos_account_id + ' \n' +
+                    'GROUP BY pos_product_id;',
+                    function (err, results, fields) {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).end();
+                        }
+
+                        invoice.items = JSON.parse(JSON.stringify(results));
+                        console.log(invoice);
+
+                        InvoiceService(invoice, 'invoice'+account.pos_account_id+'.pdf', function() {
+                            console.log("Saved invoice to invoice.pdf");
+                        }, function(error) {
+                            console.error(error);
+                        });
+
+                    }
+                );
+
             });
-            res.json(results);
+
         }
     );
 };
--- handlers/users.js
+++ handlers/users.js
@@ -2,10 +2,20 @@
 
 module.exports = function (req, res) {
     connection.query(
-      'SELECT pos_accounts.* FROM `pos_accounts` '+
-      'LEFT JOIN pos_orders ON pos_orders.pos_account_id = pos_accounts.pos_account_id '+
-      'GROUP BY name '+
-      'ORDER BY count(pos_order_id) DESC, name ASC',
+      'SELECT pos_accounts.*, saldi.saldo FROM `pos_accounts`\n' +
+        'LEFT JOIN pos_orders ON pos_orders.pos_account_id = pos_accounts.pos_account_id\n' +
+        'LEFT JOIN (SELECT pos_account_id, sum(amount) as saldo FROM (\n' +
+        'SELECT pos_account_id, sum(price) AS amount from pos_orders\n' +
+        'LEFT JOIN pos_products ON pos_products.pos_products_id = pos_orders.pos_product_id\n' +
+        'WHERE pos_order_datetime > (SELECT startdate FROM pos_accounting_periods ORDER BY pos_accounting_period_id DESC LIMIT 1)\n' +
+        'GROUP BY pos_account_id\n' +
+        'UNION ALL\n' +
+        'SELECT pos_receiving_account_id AS pos_account_id, sum(amount) as amount from pos_invoices\n' +
+        'WHERE status = \'offen\'\n' +
+        'GROUP BY pos_account_id\n' +
+        ') saldi GROUP BY pos_account_id) saldi ON pos_accounts.pos_account_id = saldi.pos_account_id\n' +
+        'GROUP BY name\n' +
+        'ORDER BY count(pos_order_id) DESC, name ASC',
         function (err, results) {
           if (err) {
             console.error(err);
@@ -14,6 +24,7 @@ module.exports = function (req, res) {
           results.map((row) => {
               row.profile_picture === '' ? row.profile_picture = process.env.AVATAR_DEFAULT : null;
               row.profile_picture = process.env.AVATAR_BASEURI + row.profile_picture;
+              row.saldo === null ? row.saldo = 0 : null;
           });
           res.json(results);
         }
