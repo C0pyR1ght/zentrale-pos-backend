5e495747774b2b7c0da437b941c3b134790e7461
Luca Feiser <luca.feiser@gmail.com>
2020-08-13 20:24:43 +0200

    invoice creation

--- handlers/createInvoices.js
+++ handlers/createInvoices.js
@@ -34,6 +34,8 @@ function generateInvoiceForPeriod(period) {
 }
 
 function generateInvoiceForAccount(account, period) {
+
+
   // calc due Date
   let dueInWeeks = 2;
   let dueDate = new Date();
@@ -47,6 +49,7 @@ function generateInvoiceForAccount(account, period) {
       logo: "https://zentrale-online.org/pos-logo.png", // todo quality
       from: "Luca Feiser\nSchafgasse 1\n63500 Seligenstadt",
       currency: "eur",
+      items: [],
       date: currentDateString,
       due_date: dueDateString,
       notes: "Abrechnungszeitraum: " +periodStartString+ " bis " +periodEndString+ ". \nZahlung Bar oder per PayPal: paypal.me/zentralepos",
@@ -56,13 +59,14 @@ function generateInvoiceForAccount(account, period) {
   connection.query(
       'SELECT count(*) AS quantity, name, price as unit_cost FROM pos_orders\n' +
       'LEFT JOIN pos_products ON pos_orders.pos_product_id = pos_products.pos_products_id\n' +
-      'WHERE pos_account_id = '+ account.pos_account_id + ' AND pos_order_datetime BETWEEN \''+period.startdate+'\' AND \''+period.enddate+'\' \n' +
-      'GROUP BY pos_product_id;', // todo order selection in Abhängigkeit der periode
+      'WHERE pos_account_id = '+ account.pos_account_id + ' AND pos_order_datetime BETWEEN \''+ period.startdate +'\' AND \''+ period.enddate +'\' \n' +
+      'GROUP BY pos_product_id;',
       function (err, results, fields) {
           if (err) {
               console.error(err);
               return res.status(500).end();
           }
+          console.log(results);
 
           invoice.items = JSON.parse(JSON.stringify(results));
           generateDBInvoice(invoice, account, period);
@@ -71,32 +75,35 @@ function generateInvoiceForAccount(account, period) {
 }
 
 function generateDBInvoice(invoice, account, period) {
+
+  const amount =  invoice.items.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.quantity * currentValue.unit_cost;
+  }, 0);
+
   connection.query(
       'INSERT INTO `zentrale-pos`.`pos_invoices`'+
       '(`pos_receiving_account_id`,'+
         '`pos_accounting_period_id`,'+
-        '`amount`,'+ // todo correct amount
+        '`amount`,'+
         '`status`)'+
         'VALUES ('+
         account.pos_account_id+','+
         period.pos_accounting_period_id+','+
-        '10, \'offen\');',
+        amount + ', \'offen\');',
       function (err, results, fields) {
           if (err) {
               console.error(err);
               return res.status(500).end();
           }
 
-          console.log(results);
-          console.log(fields);
           invoice.number = "ZENTRALE-" + results.insertId;
-          generateSingleInvoice(invoice, account); // to only if amount > 0
+          if (amount > 0) generateSingleInvoice(invoice, account);
       }
   );
 }
 
 function generateSingleInvoice(invoice, account) {
-  InvoiceService(invoice, 'invoice'+account.pos_account_id+'.pdf', function() {
+  InvoiceService(invoice, 'Rechnung-' + invoice.number + '.pdf', function() {
       console.log("Saved invoice to invoice.pdf");
   }, function(error) {
       console.error(error);
