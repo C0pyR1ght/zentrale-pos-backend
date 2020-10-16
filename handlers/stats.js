8df3a9d2852ae48cce2495677400251326324a28
C0pyR1ght <luca.feiser@gmail.com>
2020-10-17 00:37:57 +0200

    remove db name for production

--- handlers/stats.js
+++ handlers/stats.js
@@ -2,7 +2,7 @@
 
 module.exports = function (req, res) {
     connection.query(
-        'SELECT pos_products.name as title, count(*) as value FROM `zentrale-pos-dev`.pos_orders \n' +
+        'SELECT pos_products.name as title, count(*) as value FROM pos_orders \n' +
         'LEFT JOIN pos_products ON pos_products.pos_products_id = pos_orders.pos_product_id\n' +
         'group by pos_product_id;',
         function (err, results) {
